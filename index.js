4a545da5623856acdd3496791dd74202c951e90d
Francisco Lopez <francisco.lopez@morado.app>
2023-06-28 03:12:37 -0500

    Fix export

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-export * from './src';
+  // tracing.js
+  'use strict'
+  const process = require('process');
+  const opentelemetry = require('@opentelemetry/sdk-node');
+  const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+  const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+  const { Resource } = require('@opentelemetry/resources');
+  const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
+  const { SERVICE_NAME, SERVICE_ENVIRONMENT } = require('./config');
+  
+  export const initTracing = () => {
+  const exporterOptions = {
+    url: 'https://signoz.shared.morado.mx:4318/v1/traces'
+  }
+  
+  const traceExporter = new OTLPTraceExporter(exporterOptions);
+  const sdk = new opentelemetry.NodeSDK({
+    traceExporter,
+    instrumentations: [getNodeAutoInstrumentations()],
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: `${SERVICE_NAME}-${SERVICE_ENVIRONMENT}`,
+    })
+    });
+    
+    // initialize the SDK and register with the OpenTelemetry API
+    // this enables the API to record telemetry
+    sdk.start()
+    
+    // gracefully shut down the SDK on process exit
+    process.on('SIGTERM', () => {
+      sdk.shutdown()
+      .then(() => console.log('Tracing terminated'))
+      .catch((error) => console.log('Error terminating tracing', error))
+      .finally(() => process.exit(0));
+      });
+}
